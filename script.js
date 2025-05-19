fc5401d7ff54790ecafcaf3c2103d441a0b95291
Wasim Adil <waseemadilcode@gmail.com>
2025-05-20 02:54:28 +0500

    first lecture

---
+++ script.js
@@ -0,0 +1,11 @@
+/* console.log("Javascript Run in External Level")
+console.log("Waseem")*/
+
+console.log("ali")
+// console.log("hammad")
+console.log("Aham")
+
+
+// Hi I am learning console.log
+
+console.log("Hi how are you ?")
